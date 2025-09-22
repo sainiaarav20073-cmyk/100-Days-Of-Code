9de9ca8a77dd102447da444a91028e91eff6406b
sainiaarav20073-cmyk <sainiaarav20073@gmail.com>
2025-09-22 16:30:55 +0530

    Add files via upload

---
+++ day6q1.c
@@ -0,0 +1,32 @@
+
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+7 is odd
+
+Input 2:
+12
+Output 2:
+12 is even
+
+*/
+
+
+#include <stdio.h>
+int main(){
+    int number;
+    printf("enter the number : ");
+    scanf("%d", &number);
+
+
+    if  (number % 2 == 0){
+        printf("the digit is even");
+    }
+    else{
+        printf("the digit is odd");
+    }
+
+    return 0;
+}
