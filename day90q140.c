1fc4ee4dbdfd60a28a0411040846969f140b0352
sainiaarav20073-cmyk <sainiaarav20073@gmail.com>
2025-11-27 17:55:29 +0530

    Create day90q140.c

---
+++ day90q140.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+enum Gender { MALE, FEMALE, OTHER };
+
+struct Person {
+    char name[50];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+
+    // Example: setting gender to MALE
+    p.gender = MALE;
+
+    // Print gender based on enum value
+    if (p.gender == MALE)
+        printf("Male\n");
+    else if (p.gender == FEMALE)
+        printf("Female\n");
+    else
+        printf("Other\n");
+
+    return 0;
+}
