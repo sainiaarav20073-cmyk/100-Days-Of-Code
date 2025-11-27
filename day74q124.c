f5332b3cec9a312fb3594188bb81f35d47c42e21
sainiaarav20073-cmyk <sainiaarav20073@gmail.com>
2025-11-27 17:44:35 +0530

    Create day74q124.c

---
+++ day74q124.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    FILE *src, *dest;
+    char sourceFile[50], destFile[50];
+    int ch;
+
+    // Take filenames from user
+    printf("Enter source file name: ");
+    scanf("%s", sourceFile);
+
+    printf("Enter destination file name: ");
+    scanf("%s", destFile);
+
+    // Open source file in read mode
+    src = fopen(sourceFile, "r");
+    if (src == NULL) {
+        printf("Error: Could not open source file!\n");
+        return 1;
+    }
+
+    // Open destination file in write mode
+    dest = fopen(destFile, "w");
+    if (dest == NULL) {
+        printf("Error: Could not create destination file!\n");
+        fclose(src);
+        return 1;
+    }
+
+    // Copy contents character by character
+    while ((ch = fgetc(src)) != EOF) {
+        fputc(ch, dest);
+    }
+
+    // Close both files
+    fclose(src);
+    fclose(dest);
+
+    printf("File copied successfully to %s\n", destFile);
+
+    return 0;
+}
