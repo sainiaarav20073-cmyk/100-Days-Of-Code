b7064b2d69576d46a15d4f55ffda542fa9bb19d0
sainiaarav20073-cmyk <sainiaarav20073@gmail.com>
2025-11-27 17:53:15 +0530

    Create day86q136.c

---
+++ day86q136.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+enum Menu { ADD, SUBTRACT, MULTIPLY };
+
+int main() {
+    char op[20];
+    int a, b;
+
+    scanf("%s %d %d", op, &a, &b);
+
+    enum Menu choice;
+
+    if (strcmp(op, "ADD") == 0)
+        choice = ADD;
+    else if (strcmp(op, "SUBTRACT") == 0)
+        choice = SUBTRACT;
+    else
+        choice = MULTIPLY;
+
+    switch (choice) {
+        case ADD:
+            printf("%d\n", a + b);
+            break;
+
+        case SUBTRACT:
+            printf("%d\n", a - b);
+            break;
+
+        case MULTIPLY:
+            printf("%d\n", a * b);
+            break;
+    }
+
+    return 0;
+}
